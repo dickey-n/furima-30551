be92510b281e24524b823666df975885711d49d0
dickey-n <daiki.nose0501@gmail.com>
2020-10-13 19:14:48 +0900

    バリデーションの内容追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presences: true
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  zenkaku = /\A[ぁ-んァ-ン一-龥]/
+  katakana = /\A[ァ-ヶー－]+\z/
+  
+
+  validates :nickname, presence: true
+  validates :password, presence: true, format: {with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers"}
+  validates :password_confirmation, presence: true, format: {with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers"}
+
+  validates :first_name, presence: true, format: {with: zenkaku, message: "Full-width characters"}
+  validates :last_name, presence: true, format: {with: zenkaku, message: "Full-width characters"}
+
+  validates :first_name_kana, presence: true, format: {with: katakana, message: "Full-width katakana characters"}
+  validates :last_name_kana, presence: true, format: {with: katakana, message: "Full-width katakana characters"}
+
   validates :birthday, presence: true
 end
