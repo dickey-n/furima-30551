994be06e0e3e2c0e4b391b49e459eab8c7284424
dickey-n <daiki.nose0501@gmail.com>
2020-10-14 14:56:49 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  VALID_NUMBER = /\A[0-9]+\z/
+  VALID_RANGE = 
+
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :delivery_area_id, presence: true
+  validates :day_to_delivery_id, presence: true
+  validates :price, presence: true, 
+  format: {with: VALD_NUMBER, message: 'Price Half-width number'}
+  :numericality => {
+    :greater_than_or_equal_to => 300,
+    :less_than_or_equal_to => 9999999,
+    message: 'Price Out of setting range' 
+  }
 end
