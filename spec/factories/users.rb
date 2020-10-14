bbe7a8e455a08cdc8295eb0017d7a7a31fbb35b7
dickey-n <daiki.nose0501@gmail.com>
2020-10-14 12:12:50 +0900

    FactoryBotの記述方法変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname                         { Faker::Name.initials(number: 2) }
     email                            { Faker::Internet.free_email }
-    password                         { Faker::Internet.password(min_length: 6) }
+    password                         { Faker::Internet.password(min_length: 6, mix_case: true) }
     password_confirmation            { password }
     first_name                       { Gimei.last }
     last_name                        { Gimei.first }
