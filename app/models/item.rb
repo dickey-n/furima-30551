07f7692943b8dc5f9fcbf8a1c99b1d20e556bd89
dickey-n <daiki.nose0501@gmail.com>
2020-10-16 11:48:15 +0900

    バリデーションの記述変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,21 +7,23 @@ class Item < ApplicationRecord
   belongs_to_active_hash :day_to_delivery
   has_one_attached :image
 
-  validates :image, presence: true
-  validates :name, presence: true
-  validates :text, presence: true
+  with_options presence: true do
 
-  VALID_NUMBER = /[0-9]\d/.freeze
+    validates :image
+    validates :name
+    validates :text
 
-  validates :price, presence: true
-  validates :price, format: { with: VALID_NUMBER, message: 'Half width number' }
-  validates_inclusion_of :price, in: 300..9_999_999, message: 'Out of setting range'
+    VALID_NUMBER = /[0-9]\d/.freeze
 
-  validates :category_id, presence: true, numericality: { other_than: 1, message: 'Select' }
-  validates :status_id, presence: true, numericality: { other_than: 1, message: 'Select' }
-  validates :delivery_charge_id, presence: true, numericality: { other_than: 1, message: 'Select' }
-  validates :delivery_area_id, presence: true, numericality: { other_than: 1, message: 'Select' }
-  validates :day_to_delivery_id, presence: true, numericality: { other_than: 1, message: 'Select' }
+    validates :price, format: { with: VALID_NUMBER, message: 'Half width number' }
+    validates_inclusion_of :price, in: 300..9_999_999, message: 'Out of setting range'
 
-  # validates :first_name, presence: true, format: { with: VALID_NAME, message: 'Full-width characters' }
+    with_options numericality: { other_than: 1, message: 'Select' } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_charge_id
+      validates :delivery_area_id
+      validates :day_to_delivery_id
+    end
+  end
 end
