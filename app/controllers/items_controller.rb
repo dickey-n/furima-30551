ea735a632fc2e7a23df78884e2e05e954ba57a40
dickey-n <daiki.nose0501@gmail.com>
2020-10-16 16:18:55 +0900

    コントローラのアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,10 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def show
+    @item = Item.new(params[:id])
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
