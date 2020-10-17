364d0e9ce64d0ecf744ffc2d8012bbeed795995e
dickey-n <daiki.nose0501@gmail.com>
2020-10-17 11:38:25 +0900

    itemコントローラーの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,13 @@ class ItemsController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
