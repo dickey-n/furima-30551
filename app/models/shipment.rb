770830f5fd1a4e6ee0eb0e3b7b2ba29f61e14adc
dickey-n <daiki.nose0501@gmail.com>
2020-10-19 16:06:23 +0900

    バリデーションの設定

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,3 +1,15 @@
 class Shipment < ApplicationRecord
   belongs_to :order
+
+  with_options presence: true do
+    validates :post_code
+    validates :city_town
+    validates :address
+
+    VALID_POST_CODE = /\A\d{3}[-]\d{4}\z/
+    VALID_PHONE_NUMBER = /\A\d{10}$|^\d{11}\z/
+
+    validates :prefecture_id, format: {with:VALID_POST_CODE, message: "Postal code Input correctly"}
+    validates :phone_number, format: {with: VALID_PHONE_NUMBER}
+  end
 end
